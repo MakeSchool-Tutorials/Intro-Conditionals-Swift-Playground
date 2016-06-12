21d8e7617b8ad66ac9ff09341c923d989a7d6c1e
Dion Larson <me@dionlarson.com>
2016-06-12 11:40:17 -0700

    Initial commit

---
+++ Conditionals.playground/Pages/06-Extending-the-challenge.xcplaygroundpage/Contents.swift
@@ -0,0 +1,130 @@
+//: [Previous](@previous)
+/*:
+ 
+ # Make School: Design, Code & Ship
+ 
+ Let's add a few more rules to the challenge!
+ 
+ - callout(Challenge): Modify the `for` loop below to...
+ 
+     - Print _Make_ is the number is divisible by 3
+     - Print _School_ is the number is divisible by 5
+     - Print _Make School_ is the number is divisible by 3 and 5
+     - Print _Design_ is the number is divisible by 7
+     - Print _Code_ is the number is divisible by 8
+     - Print _Ship_ is the number is divisible by 13
+     - Otherwise, print the number
+ 
+ */
+for i in 1...100 {
+    // Implement Make School: Design, Code & Ship here!
+    
+}
+/*:
+ 
+ - callout(Hint): The logic in the rules might not be in the right order for your `if` statements!
+ 
+ ## Your console should display the following:
+ 
+     1
+     2
+     Make
+     4
+     School
+     Make
+     Design
+     Code
+     Make
+     School
+     11
+     Make
+     Ship
+     Design
+     Make School
+     Code
+     17
+     Make
+     19
+     School
+     Make
+     22
+     23
+     Make
+     School
+     Ship
+     Make
+     Design
+     29
+     Make School
+     31
+     Code
+     Make
+     34
+     School
+     Make
+     37
+     38
+     Make
+     School
+     41
+     Make
+     43
+     44
+     Make School
+     46
+     47
+     Make
+     Design
+     School
+     Make
+     Ship
+     53
+     Make
+     School
+     Design
+     Make
+     58
+     59
+     Make School
+     61
+     62
+     Make
+     Code
+     School
+     Make
+     67
+     68
+     Make
+     School
+     71
+     Make
+     73
+     74
+     Make School
+     76
+     Design
+     Make
+     79
+     School
+     Make
+     82
+     83
+     Make
+     School
+     86
+     Make
+     Code
+     89
+     Make School
+     Design
+     92
+     Make
+     94
+     School
+     Make
+     97
+     Design
+     Make
+     School
+ 
+ */
